b966655fe26a80e0b126e99b720a99d3a468ca5c
wangzz28 <wangzz28@lenovo.com>
2025-04-24 17:09:37 +0800

    update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import TestUI from "@lzwui/components";
 import "@lzwui/theme-chalk/index.scss"
 import Preview from "./preview/index.vue"; // 引入预览功能的组件
 import "highlight.js/styles/base16/summerfruit-light.css"; // 主题
-import hljsVuePlugin from "@highlightjs/vue-plugin";    
+import * as hljsVuePlugin  from '@highlightjs/vue-plugin'
 export default {
     ...DefaultTheme,
     enhanceApp: async ({ app, router, siteData }) => {
