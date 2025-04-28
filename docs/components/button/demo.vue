632b99cea1e2be3462e78adb9c43b47fb60602f9
wangzz28 <wangzz28@lenovo.com>
2025-04-28 17:10:12 +0800

    button 文档

--- docs/components/button/demo.vue
+++ docs/components/button/demo.vue
@@ -1,24 +1,23 @@
 <template>
   <div>
+    <h2>类型</h2>
     <t-button type="primary">primary</t-button>
     <t-button type="success">success</t-button>
     <t-button type="info">info</t-button>
     <t-button type="warning">warning</t-button>
     <t-button type="danger">danger</t-button>
   </div>
+
   <div style="margin-top: 20px;">
+    <h2>icon</h2>
+    <t-button type="primary" circle icon="happy-filled"></t-button>
     <t-button type="primary" icon="feel-at-ease-filled">primary2</t-button>
   </div>
-  <div style="margin-top: 20px;">
-    <t-button type="primary" circle icon="happy-filled"></t-button>
-  </div>
 
-  <div>尺寸</div>
   <div style="margin-top: 20px;">
+    <h2>尺寸</h2>
     <t-button type="primary">默认尺寸</t-button>
-
     <t-button type="primary" size="small">small</t-button>
     <t-button type="primary" size="mini">mini</t-button>
   </div>
-  <div style="margin-top: 20px;"></div>
 </template>
--- docs/components/button/index.md
+++ docs/components/button/index.md
@@ -7,3 +7,11 @@ import demo1 from './demo.vue'
 <preview comp-name="button" demo-name="demo">
   <demo1/>
 </preview>
+
+# props
+
+| 参数 | 说明                                                                    | 类型   | 默认值    |
+| ---- | ----------------------------------------------------------------------- | ------ | --------- |
+| type | 按钮类型，可选值为 `primary` `success` `warning` `danger` `info` `text` | string | `default` |
+| size | 按钮大小，可选值为 `small` `mini` `defualt`                             | string | `default` |
+| icon | 按钮图标，支持设置图标类型，例如：`icon="search"`                       | string | -         |
