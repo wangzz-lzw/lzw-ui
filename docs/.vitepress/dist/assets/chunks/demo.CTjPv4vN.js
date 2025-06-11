af8a3248bb51706dce1fbf4570bb20d58c9c42f6
wangzz28 <wangzz28@lenovo.com>
2025-06-11 12:01:24 +0800

    gitignore

--- docs/.vitepress/dist/assets/chunks/demo.CTjPv4vN.js
+++
@@ -1,7 +0,0 @@
-const t=`<template>
-  <t-button type="primary">primary</t-button>
-  <t-button type="success">success</t-button>
-  <t-button type="info">info</t-button>
-  <t-button type="primary">primary</t-button>
-</template>
-`;export{t as default};
